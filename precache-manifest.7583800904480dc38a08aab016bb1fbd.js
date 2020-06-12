57bcaf7c8939e27e8c4056aeaa16db2c6b078028
Hirenkumar Khant <hr266981@dal.ca>
2020-06-12 01:48:36 -0300

    Updates

---
+++ precache-manifest.7583800904480dc38a08aab016bb1fbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4c30a351a499d65327a00d6994f217",
+    "url": "/greenkart/index.html"
+  },
+  {
+    "revision": "37a8928d572fad7f07b3",
+    "url": "/greenkart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "91700952157f17caa15c",
+    "url": "/greenkart/static/js/2.316240f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/greenkart/static/js/2.316240f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a8928d572fad7f07b3",
+    "url": "/greenkart/static/js/main.3ea60881.chunk.js"
+  },
+  {
+    "revision": "5b2b3b32747c20351925",
+    "url": "/greenkart/static/js/runtime-main.71df108d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/greenkart/static/media/logo.5d5d9eef.svg"
+  }
+]);
