e23d711978ce2f8b9a5fdb5df75b7b34346677d0
Hirenkumar Khant <hr266981@dal.ca>
2020-06-14 14:52:42 -0300

    login page done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import LoginPage from './Pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hiren Khant</h2>
+      <Navbar />
+      <BrowserRouter>
+        <Switch>
+            <Route exact path='/login' component={LoginPage} /> 
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/LoginPage.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import LoginForm from '../Forms/login';
+import './LoginPage.css'
+
+class LoginPage extends Component{
+    
+    render(){
+        return(
+            <>
+                <h1>Login</h1>
+                <div className="form" >
+                    <LoginForm/>
+                    <br></br>
+                    <span>Don't have an account? <a href="/signup">Sign up</a></span>
+                </div>
+                <div className="greeting">
+                    <h4>How are you doing today?</h4>
+                    <span>We are happy to have you on our team of gardeners, home planters, and tree-huggers!</span>
+                </div>
+            </>
+        );
+    }
+}
+
+export default LoginPage;
