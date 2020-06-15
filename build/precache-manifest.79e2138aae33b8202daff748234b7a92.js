a357715372e18fd6337dcbd507eb4daeefd222a5
Hirenkumar Khant <hr266981@dal.ca>
2020-06-14 22:50:15 -0300

    ran build command

---
+++ build/precache-manifest.79e2138aae33b8202daff748234b7a92.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a4a2966e1f3b37cd00a9269e255df4",
+    "url": "/greenkart/index.html"
+  },
+  {
+    "revision": "763e51e04966bfe29f0b",
+    "url": "/greenkart/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d9a260606ff905b6e263",
+    "url": "/greenkart/static/css/main.5acf93cc.chunk.css"
+  },
+  {
+    "revision": "763e51e04966bfe29f0b",
+    "url": "/greenkart/static/js/2.82d66950.chunk.js"
+  },
+  {
+    "revision": "0df0222e6aeae3a3b26319838c960dde",
+    "url": "/greenkart/static/js/2.82d66950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a260606ff905b6e263",
+    "url": "/greenkart/static/js/main.6c544da9.chunk.js"
+  },
+  {
+    "revision": "5b2b3b32747c20351925",
+    "url": "/greenkart/static/js/runtime-main.71df108d.js"
+  },
+  {
+    "revision": "664aed2acacf8c58fadd62a009784754",
+    "url": "/greenkart/static/media/GreenKartLogo_transparent.664aed2a.png"
+  },
+  {
+    "revision": "015d76c8e76eeac786ee1ded13ae0e3d",
+    "url": "/greenkart/static/media/img2.015d76c8.jpg"
+  },
+  {
+    "revision": "41887919b8f0b4602f54f22ca90e78e8",
+    "url": "/greenkart/static/media/img3.41887919.jpg"
+  },
+  {
+    "revision": "02f52944430ff396a641c56834a9ed7f",
+    "url": "/greenkart/static/media/img4.02f52944.jpg"
+  },
+  {
+    "revision": "d987d01c5f20f7c98e3b8ea20a2ecbb7",
+    "url": "/greenkart/static/media/img5.d987d01c.jpg"
+  },
+  {
+    "revision": "595326afc27d9324e2bae5cfaba79198",
+    "url": "/greenkart/static/media/img6.595326af.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/greenkart/static/media/logo.5d5d9eef.svg"
+  }
+]);
