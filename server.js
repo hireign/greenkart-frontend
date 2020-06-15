e2a51137d742fcc61dfa1ba1a3a55d4ff8f998fb
Hirenkumar Khant <hr266981@dal.ca>
2020-06-15 18:50:00 -0300

    modified files

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(__dirname+'/build/'));
+//app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '/build/index.html'));
   });
 app.listen(port);
